d130c26783c46f03fbcbeff43cea1646e4ca231e
Gabriel Teles de Faria <gabrielfariateles@gmail.com>
2025-07-23 16:16:21 -0300

    feat: unread notification ping on notifications component, unread badge to notification item component.

--- websockets-notification-web/src/components/Notifications.tsx
+++ websockets-notification-web/src/components/Notifications.tsx
@@ -1,30 +1,42 @@
 import { Bell } from "lucide-react";
 import { Button } from "./ui/button";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useSocket } from "@/hooks/useSocket";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { formatDistanceToNow } from "date-fns";
-import { ptBR } from "date-fns/locale";
 
 type Notification = {
+    id: string;
     author: {
         name: string;
         avatar_url?: string;
     };
     message: string;
     created_at: string;
+    read: boolean;
 };
 
-const NotificationItem = ({ data }: { data: Notification }) => {
+interface NotificationItemProps extends Notification {
+    onMarkAsRead: (id: string) => void;
+}
+
+const NotificationItem = ({ data }: { data: NotificationItemProps }) => {
     return (
-        <li className="w-full flex gap-3 hover:bg-accent transition-colors p-3 rounded">
-            <Avatar>
-                <AvatarFallback>
-                    {data.author.name.charAt(0).toUpperCase()}
-                </AvatarFallback>
-                <AvatarImage src={data.author.avatar_url} />
-            </Avatar>
+        <li
+            onClick={() => {
+                data.onMarkAsRead(data.id);
+            }}
+            className="w-full flex gap-3 hover:bg-accent transition-colors p-3 rounded"
+        >
+            <div className="relative">
+                <Avatar>
+                    <AvatarFallback>
+                        {data.author.name.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                    <AvatarImage src={data.author.avatar_url} />
+                </Avatar>
+            </div>
 
             <div className="space-y-2">
                 <div className="flex text-xs gap-1.5 items-center">
@@ -35,9 +47,13 @@ const NotificationItem = ({ data }: { data: Notification }) => {
                     <p className="text-muted-foreground">
                         {formatDistanceToNow(data.created_at, {
                             addSuffix: true,
-                            locale: ptBR,
                         })}
                     </p>
+                    {!data.read && (
+                        <span className="text-[9px] py-0.5 px-1 bg-lime-300/20 rounded">
+                            Unread
+                        </span>
+                    )}
                 </div>
                 <p className="text-sm">{data.message}</p>
             </div>
@@ -48,12 +64,14 @@ const NotificationItem = ({ data }: { data: Notification }) => {
 export function Notifications() {
     const [notifications, setNotifications] = useState<Notification[]>([
         {
+            id: "uuid",
             author: {
                 name: "Gabriel Teles",
                 avatar_url: "https://github.com/GabrielFTeles.png",
             },
             message: "Try adding a new notification!",
-            created_at: "2025-04-13",
+            created_at: new Date().toString(),
+            read: false,
         },
     ]);
 
@@ -61,17 +79,40 @@ export function Notifications() {
         setNotifications((prev) => [newNotification, ...prev]);
     });
 
+    const markAsRead = (id: string) => {
+        setNotifications((prev) =>
+            prev.map((notification) =>
+                notification.id === id
+                    ? { ...notification, read: true }
+                    : notification
+            )
+        );
+    };
+
+    const hasUnread = useMemo(
+        () => notifications.some((notification) => !notification.read),
+        [notifications]
+    );
+
     return (
         <Popover defaultOpen>
             <PopoverTrigger asChild>
-                <Button variant="outline" className="h-12 w-12">
+                <Button variant="outline" className="h-12 w-12 relative">
                     <Bell className="size-5" />
+                    {hasUnread && (
+                        <div className="rounded-full size-3 p-1 bg-zinc-900 right-[13px] top-2.5 absolute grid place-items-center">
+                            <div className="size-2 animate-pulse bg-lime-400 rounded-full absolute" />
+                        </div>
+                    )}
                 </Button>
             </PopoverTrigger>
             <PopoverContent side="right" className="w-[350px]">
                 <ul className="flex flex-col items-center">
                     {notifications.map((notification, index) => (
-                        <NotificationItem key={index} data={notification} />
+                        <NotificationItem
+                            key={index}
+                            data={{ ...notification, onMarkAsRead: markAsRead }}
+                        />
                     ))}
                 </ul>
             </PopoverContent>
